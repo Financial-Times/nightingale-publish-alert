a3120d878a6564803a9fe106dc6a6a18e4a36a86
radiodario <dario.villanueva@gmail.com>
2015-07-23 15:32:08 +0100

    have a health check

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 require('dotenv').load();
 
-var poller = require('./src/poller');
+var cors = require('cors');
+var express = require('express');
+var app = express();
+var logger = require('./src/logger');
 
+
+app.get('/', function (req, res) {
+  res.send('Nightingale Publish Alert Service OK!');
+});
+
+
+var poller = require('./src/poller');
 
 setInterval(function() {
   poller.poll();
 }, 1 * 60 * 1000);
 
 poller.poll();
+
+
+
+var server = app.listen(process.env.PORT || 3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  logger.log('info', 'Nightingale Publish Alert health check at http://%s:%s', host, port);
+
+});
