9daaf94e16996d5e5cf4c146a78d66bde4a72f94
jonathan-best <jonathan.best@ft.com>
2017-07-19 12:01:28 +0100

    Add secret squirrel  🐿2.5.10

---
+++ secret-squirrel.js
@@ -0,0 +1,13 @@
+module.exports = {
+	files: {
+		allow: [],
+		allowOverrides: []
+	},
+	strings: {
+		deny: [],
+		denyOverrides: [
+			'2c67f078-2c6d-11e5-8613-e7aedbb7bdb7', // test/notifier.js:38|70|76
+			'ac2da5e8-2c89-11e5-acfb-cbd2e1c81cca' // test/notifier.js:43
+		]
+	}
+};
